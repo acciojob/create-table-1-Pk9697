0018931e3b43526100779644948722b1abee4bcf
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-07 21:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const table=document.getElementById('sampleTable')
 function insert_Row() {
     //Write your code here
-  
-  
+		const newTableRow=`
+			<tr>
+				<td>New Cell1</td> 
+				<td>New Cell2</td>
+			</tr> 
+		`
+	  table.innerHTML=newTableRow+table.innerHTML
 }

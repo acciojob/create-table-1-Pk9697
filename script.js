fa03188684457389e7c57ebd1af2c99f34ac9c8c
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-07 22:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const table=document.getElementById('sampleTable')
 function insert_Row() {
     //Write your code here
-		const newTableRow=`
-			<tr>
-				<td>New Cell1</td> 
-				<td>New Cell2</td>
-			</tr> 
-		`
-	  table.innerHTML=newTableRow+table.innerHTML
+	const newRow=table.insertRow(0)
+
+	const newCell1=newRow.insertCell(0)
+	const newCell2=newRow.insertCell(1)
+
+	newCell1.innerHTML="New Cell1"
+	newCell2.innerHTML="New Cell2"
+	  
 }
+// function insert_Row() {
+//     //Write your code here
+// 		const newTableRow=`
+// 			<tr>
+// 				<td>New Cell1</td> 
+// 				<td>New Cell2</td>
+// 			</tr> 
+// 		`
+// 	  table.innerHTML=newTableRow+table.innerHTML
+// }
